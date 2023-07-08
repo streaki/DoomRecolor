2b420edcba59306f32309289e549737726c336dc
streaki <whystreaki@gmail.com>
2023-07-08 17:28:24 +0200

    Add multiply operator

--- recolor.py
+++ recolor.py
@@ -5,19 +5,34 @@ import cv2
 
 
 def main(args: Namespace) -> None:
-    print(args.images)
-    print(args.color_image)
-    print(args.multiply)
+    # print(args.images)
+    # print(args.color_image)
+    # print(args.multiply)
     color_image = cv2.imread(args.color_image)
     for filename in args.images:
         value_image = cv2.imread(filename)
+        # convert both images to HSV
         value_hsv = cv2.cvtColor(value_image, cv2.COLOR_BGR2HSV_FULL)
         color_hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV_FULL)
-        color_hsv[:,:,2] = value_hsv[:,:,2]
+        # trust me it looked like garbage with python's ternary op
+        if args.multiply:
+            color_hsv[:,:,2] = multiply_value(color_hsv, value_hsv)
+        else:
+            color_hsv[:,:,2] = value_hsv[:,:,2]
         result = cv2.cvtColor(color_hsv, cv2.COLOR_HSV2BGR_FULL)
-        # print(len(color_hsv[0][0]))
         cv2.imshow('foo', result)
         cv2.waitKey()
+
+
+def multiply_value(img1: cv2.Mat, img2: cv2.Mat) -> cv2.Mat:
+    """
+    Combine the value channels of two images as if we are multiplying floats of range 0..1
+
+    Step 1: force float conversion by multiplying by 1f
+    Step 2: multiply values by each other
+    Step 3: floor divide by 255 to get them back into unsigned byte range
+    """
+    return cv2.multiply(img1[:,:,2] * 1., img2[:,:,2] * 1.) // 255
 
 
 def parse_arguments() -> Namespace:
