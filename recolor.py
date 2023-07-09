36303a38529f42f6759d19e51bc32b5ec574baec
streaki <whystreaki@gmail.com>
2023-07-09 12:33:01 +0200

    Add an option to scale brightness of multiplied images

--- recolor.py
+++ recolor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from argparse import Namespace, ArgumentParser, BooleanOptionalAction, REMAINDER
+from argparse import Namespace, ArgumentParser, REMAINDER
 import cv2
 from math import ceil
 from os import path
@@ -16,11 +16,12 @@ def main(args: Namespace) -> None:
         # fit the output image to the value image size
         color_hsv = resize_image(color_hsv, value_hsv.shape)
         # trust me it looked like garbage with python's ternary op
-        if args.multiply:
-            color_hsv[:,:,2] = multiply_value(color_hsv, value_hsv)
+        if args.multiply >= 0:
+            color_hsv[:,:,2] = multiply_and_brighten(color_hsv, value_hsv, args.multiply)
         else:
             color_hsv[:,:,2] = value_hsv[:,:,2]
         result = cv2.cvtColor(color_hsv, cv2.COLOR_HSV2BGR_FULL)
+        # add alpha channel from original value image if present
         if value_image.shape[2] > 3:
             b, g, r = cv2.split(result)
             _, _, _, a = cv2.split(value_image)
@@ -31,12 +32,28 @@ def main(args: Namespace) -> None:
 def multiply_value(img1: cv2.Mat, img2: cv2.Mat) -> cv2.Mat:
     """
     Combine the value channels of two images as if we are multiplying floats of range 0..1
+    The result image is pretty dark, so it needs to be brightened by hand
 
     Step 1: force float conversion by multiplying by 1f
     Step 2: multiply values by each other
     Step 3: floor divide by 255 to get them back into unsigned byte range
     """
     return cv2.multiply(img1[:,:,2] * 1., img2[:,:,2] * 1.) // 255
+
+
+def multiply_and_brighten(img1: cv2.Mat, img2: cv2.Mat, brightness: int) -> cv2.Mat:
+    """
+    Combine the value channels of two images as if we are multiplying floats of range 0..1,
+    but brighten them so they are semi-usable out of the box
+
+    Step 1: force float conversion by multiplying by 1f
+    Step 2: multiply values by each other
+    Step 3: floor divide by 128 to make them not as overexposed
+    Step 4: clamp to 0..255
+    """
+    res = cv2.multiply(img1[:,:,2] * 1., img2[:,:,2] * 1.) // (255 - brightness)
+    res[res > 255] = 255
+    return res
 
 
 def resize_image(image: cv2.Mat, dest_size: tuple):
@@ -61,10 +78,14 @@ def parse_arguments() -> Namespace:
     parser.add_argument(
         '-m',
         '--multiply',
-        type=bool,
-        default=False,
-        action=BooleanOptionalAction,
-        help="Multiply the two images' values together instead of replacing one with the other",
+        type=int,
+        default=-1,
+        const=0,
+        action='store',
+        nargs='?',
+        help='''
+Multiply the two images' values together instead of replacing one with the other.
+Optionally specify a number between 0 and 255 to brighten it.''',
     )
     parser.add_argument(
         '-o',
